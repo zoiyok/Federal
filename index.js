b57f4950b95f75c3ad8e6d172c1f418760252a04
zoiyok <78409515+zoiyok@users.noreply.github.com>
2022-02-11 22:18:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ function open_passage() {
 function open_passage_2021_27() {
     window.location.href="2021_27.html";
 }
+function open_2022_1_president() {
+    window.location.href="2022_1_president.html";
+}
