47c37ad743d01d2daa9f7ba36d008ad382a09637
dyh <1358366+dyh@users.noreply.github.com>
2022-02-12 10:25:50 +0800

    提交信息

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ function open_passage_2021_27() {
 function open_2022_1_president() {
     window.location.href="2022_1_president.html";
 }
+function ShowHome() {
+    window.location.href="index.html";
+}
+
+var tt = 40;
+window.onscroll = function(){
+    var t = document.documentElement.scrollTop || document.body.scrollTop;
+    if (t > tt) {
+        lplp.style.height = "50px";
+        lplp.style.lineHeight = "50px";
+        logoid.style.paddingBottom = "15px";
+    } else if (t < tt) {
+        lplp.style.lineHeight = "70px";
+        lplp.style.height = "70px";
+        logoid.style.paddingBottom = "20px";
+    }
+}
+ 
