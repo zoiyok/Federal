291dd9a231c58a460edd341cf9d9b24e391ada6e
dyh <1358366+dyh@users.noreply.github.com>
2022-02-15 20:09:18 +0800

    none

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ function open_2022_1_president() {
 function open_2022_2() {
     window.location.href="2022_2.html";
 }
+function open_2022_1_law() {
+    window.location.href="2022_1_law.html";
+}
 function ShowHome() {
     window.location.href="index.html";
 }
